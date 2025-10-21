892faa309803301698782c07294defbe903783b4
louis-souillard <rdxlouis@gmail.com>
2025-10-21 11:44:47 +0200

    Merge louis bouton developpeurs porte enigme1

--- lib/pages/enigme_2/MenhirsEnigme.dart
+++ lib/pages/enigme_2/MenhirsEnigme.dart
@@ -2,6 +2,7 @@ import 'dart:ui';
 import 'package:flutter/material.dart';
 import '../../widgets/timer_button.dart';
 import '../enigme_3/autelMenhirs.dart';
+import '../home.dart';
 
 class MenhirsEnigme extends StatefulWidget {
   const MenhirsEnigme({super.key});
@@ -323,6 +324,35 @@ class _Enigme2MenhirsPageState extends State<MenhirsEnigme>
                     ),
                   ],
                 ),
+              ),
+            ),
+          if (isDevMode)
+            Positioned(
+              bottom: 30,
+              right: 30,
+              child: ElevatedButton.icon(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black.withOpacity(0.7),
+                  foregroundColor: Colors.white,
+                  padding:
+                  const EdgeInsets.symmetric(horizontal: 18, vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                icon: const Icon(Icons.arrow_forward),
+                label: const Text(
+                  "Page suivante (Dev)",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                onPressed: () {
+
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(
+                        builder: (_) => const autelMenhirPage()),
+                  );
+                },
               ),
             ),
         ],
